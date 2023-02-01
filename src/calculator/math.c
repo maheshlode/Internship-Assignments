131e29c1f8488cf9a6717e17b2c7c029949f4188
maheshlode <maheshlode55@gmail.com>
2023-02-01 12:32:24 +0530

    changes

--- src/calculator/math.c
+++ src/calculator/math.c
@@ -1,14 +1,14 @@
 int multiplication(int num1, int num2)
 {
-    return (num*num);
+    return (num1*num2);
 }
 
 int division(int num1, int num2)
 {
-    return(num1/num2);
+    return (num1/num2);
 }
 
 int remainder(int num1, int num2)
 {
-    return(num1%num2);
+    return (num1%num2);
 }
