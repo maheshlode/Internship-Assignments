72d68a8142516fd4279adc28c62e8fedbc183d2f
maheshlode <maheshlode55@gmail.com>
2023-02-01 12:14:49 +0530

    mahesh commited

---
+++ src/calculator/math.c
@@ -0,0 +1,14 @@
+int multiplication(int num1, int num2)
+{
+    return (num*num);
+}
+
+int division(int num1, int num2)
+{
+    return(num1/num2);
+}
+
+int remainder(int num1, int num2)
+{
+    return(num1%num2);
+}
