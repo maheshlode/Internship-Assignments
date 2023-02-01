024b4801f50437e6ed931dc7aaf9ad3e232299f4
ShivanshBharatBhushanSharma <shivansh.sharmas1999@gmail.com>
2023-02-01 12:21:21 +0530

    shivansh code commited

---
+++ src/calculator/math.c
@@ -0,0 +1,17 @@
+#include<math.h>
+
+int square(int num){
+    return (num * num);
+}
+
+int squareRoot(int num){
+    return power(num , 1/2);
+}
+
+int cube(int num){
+    return (num * num * num);
+}
+
+int cubeRoot(int num){
+    return pow(num , 1/3);
+}
